afce6ed822f58cc33061342facff2653348652c7
Dragonabysm <90159398+Dragonabysm@users.noreply.github.com>
2022-12-15 14:48:16 -0300

    About and menu Bar

--- main.py
+++ main.py
@@ -1,18 +1,85 @@
 from tkinter.ttk import Label, Frame, Button, Entry, Style
-from tkinter import Tk, StringVar, Toplevel
+from tkinter import Tk, StringVar, Menu
 from senders import sendLogin, sendRegistration
+from webbrowser import open_new
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
 
 
 class ApplicationLogin():
     """ The base of this code """
+
     def __init__(self, geometry: str):
         self.screen = Tk('Application', sync=True)
         self.screen.geometry(geometry)
         self.screen.title('Application Login')
         self.screen.resizable(False, False)
-        self.setupScreenStyle()
+        self.menuBar = Menu(self.screen, background='gray')
 
-    def setupScreenStyle(self):
+        console = Console()
+        console.clear(True)
+        title = '[b purple]Welcome to my first portfolio project[/]'
+        textBody = "     Yes, it is a [b i blue]login screen[/], although simple it is the first of many. It was made entirely with :snake: [green]python and some libraries.[/] \n \n     Hmm, I haven't said my name yet... Nice to meet you, I'm Higor, a [b i yellow on black]Brazilian[/] 12 year old student. Lately I've been studying [green]python[/], but I intend to study \n:crab: [red]rust[/] soon (it's the language of the future!). If you are interested, visit my [blue]github[/]: [b red]https://github.com/Dragonabysm[/] All my projects will be concentrated there."
+        print(Panel(textBody, title=title, title_align='center'))
+
+        self.menuAcess = Menu(
+            self.menuBar,
+            tearoff=0
+        )
+
+        self.menuAcess.add_command(
+            label='Cadastro',
+            command=self.makeRegistration
+        )
+
+        self.menuAcess.add_command(
+            label='Login',
+            command=self.makeLogin
+        )
+
+        self.menuAcess.add_command(
+            label='Initial',
+            command=lambda: self.setupScreenStyle(init=False)
+        )
+
+        self.menuBar.add_cascade(
+            menu=self.menuAcess,
+            label='Acessar'
+        )
+
+        self.menuProject = Menu(
+            self.menuBar,
+            tearoff=0
+        )
+
+        self.menuProject.add_command(
+            label='Github',
+            command=lambda: open_new(
+                'https://github.com/Dragonabysm/Login-system')
+        )
+
+        self.menuProject.add_separator()
+
+        self.menuProject.add_command(
+            label='Sobre o projeto',
+            command=lambda: open_new(
+                'SistemaLoginCadastro/website/index.html')
+        )
+
+        self.menuBar.add_cascade(
+            menu=self.menuProject,
+            label='Project'
+        )
+
+        self.screen.config(menu=self.menuBar)
+
+        self.setupScreenStyle(init=True)
+
+    def setupScreenStyle(self, init: bool):
+        if not init:
+            self.frame.destroy()
+
         self.frame = Frame(self.screen)
 
         # Style section
@@ -122,7 +189,8 @@ class ApplicationLogin():
         self.sendButton = Button(
             self.frame,
             default='normal',
-            command=lambda: sendRegistration(self.email.get(), self.password.get(), self.screen, self.sendButton),
+            command=lambda: sendRegistration(
+                self.email.get(), self.password.get(), self.screen, self.sendButton),
             text='Cadastrar'
         )
 
@@ -189,7 +257,8 @@ class ApplicationLogin():
         self.sendButton = Button(
             self.frame,
             default='normal',
-            command=lambda: sendLogin(self.email.get(), self.password.get(), self.screen, self.frame),
+            command=lambda: sendLogin(
+                self.email.get(), self.password.get(), self.screen, self.frame),
             text='Login'
         )
 
@@ -198,5 +267,7 @@ class ApplicationLogin():
         self.sendButton.pack(ipadx=100, ipady=10)
         self.frame.pack()
 
-app = ApplicationLogin('500x400+100+100')
-app.runApplication()
+
+if __name__ == '__main__':
+    app = ApplicationLogin('500x400+100+100')
+    app.runApplication()
